083f4f240a315a228781b762371f914f8d4163a0
Ch3dd4rGames <126571935+Ch3dd4rGames@users.noreply.github.com>
2024-04-30 09:39:25 +0200

    Add files via upload

---
+++ Code Snipets/C#/Other/Class B/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserte el primer número: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Inserte el segundo número: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Escoja una de las siguientes operaciones: ");
+            Console.WriteLine("1.- Mayor y menor.");
+            Console.WriteLine("2.- Distancia. ");
+            Console.WriteLine("3.- Media.");
+            int d = Convert.ToInt32(Console.ReadLine());
+            switch (d)
+            {
+                case 1:
+                    if (n1 > n2)
+                    {
+                        Console.WriteLine(n1 + " es el número mayor y " + n2 + " el menor.");
+                    }
+                    else if (n1 < n2)
+                    {
+                        Console.WriteLine(n2 + " es el número mayor y " + n1 + " el menor.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ambos números son iguales.");
+                    }
+                    break;
+            
+                case 2:
+                    if (n1 >= n2)
+                    {
+                        Console.WriteLine("La distancia entre ambos números es de " + (n1 - n2) + ".");
+                    }
+                    else
+                    {
+                        Console.WriteLine("La distancia entre ambos números es de " + (n2 - n1) + ".");
+                    }
+                    break;
+
+                case 3:
+                    Console.WriteLine("La media entre ambos números es de " + ((n1 + n2) / 2) + ".");
+                    break;
+
+                default:
+                    Console.WriteLine("No has metido una operación correcta.");
+                    break;
+            }
+        }
+    }
+}
+
+
---
+++ Code Snipets/C#/Other/Class B/Program2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int numeroPensado = 0;
+            double decimalAleatorio = 0;
+            while (true)
+            {
+                numeroPensado = random.Next(1, 11);
+                decimalAleatorio = random.NextDouble() * 7;
+                Console.WriteLine(numeroPensado + "     " + decimalAleatorio);
+                Console.ReadLine();
+            }
+        }
+    }
+}
